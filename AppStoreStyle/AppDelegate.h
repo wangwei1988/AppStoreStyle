a8cd9addb53b975b95e6626ae7b55a7817cec8ad
wangwei1988 <mac@macdeMacBook-Air.local>
2017-11-21 15:39:56 +0800

    Initial Commit

---
+++ AppStoreStyle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppStoreStyle
+//
+//  Created by mac on 2017/11/21.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppStoreStyle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppStoreStyle
+//
+//  Created by mac on 2017/11/21.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
